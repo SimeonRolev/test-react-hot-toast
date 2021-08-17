2f8b2f010557e4819effd5554aa6116cbc427953
Simeon Rolev <srolev@nemetschek.bg>
2021-08-17 16:32:06 +0300

    Advance mocked timers

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,55 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Toaster } from 'react-hot-toast';
-import { notify } from './App';
 
-test('closes notification', async () => {
-  render(<Toaster />);
-  notify()
-  const message = await screen.findByText('Example');
-  expect(message).toBeInTheDocument();
+beforeEach(() => {
+  jest.useFakeTimers()
+})
 
-  const closeButton = await screen.findByRole('button', { name: /close/i })
-  userEvent.click(closeButton)
-  await waitFor(() => {
-    console.log('retry')
-    expect(screen.queryByText(/example/i)).not.toBeInTheDocument()
-  }, { timeout: 10000 })
-}, 12000);
+afterEach(() => {
+  jest.runOnlyPendingTimers()
+  jest.useRealTimers()
+})
+
+function setup() {
+  const { Toaster } = require('react-hot-toast');
+  const toast = require('react-hot-toast').default;
+
+  return {
+    toast,
+    ...render(<Toaster />)
+  };
+}
+
+test('fake timeouts', async () => {
+  const { toast } = setup()
+
+  act(() => {
+    toast(
+      (t) => {
+        console.log(t)
+        return <div>
+              Example
+              <button onClick={() => toast.dismiss(t.id)} title={'close'}>
+                Close
+              </button>
+          </div>
+        }
+    );
+  })
+
+  expect(await screen.findByText('Example')).toBeInTheDocument();
+  userEvent.click(await screen.findByRole('button', { name: /close/i }))
+
+  // All the setTimeouts that are called
+  setTimeout.mock.calls.forEach(([call, timeout]) => {
+    console.log(call.toString(), timeout);
+  });
+
+  // Expected: advance the setTimeout started by DISMISS_TOAST event to finish
+  // And the addToRemoveQueue method to call the REMOVE_TOAST event, and delete from DOM
+  // Actual: many re-renders + infinite test
+
+  jest.advanceTimersByTime(4000);
+
+  expect(screen.queryByText(/example/i)).not.toBeInTheDocument()
+})
